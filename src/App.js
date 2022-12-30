613b7d94e08723ad945184cef4a0a74ec79282b4
Corso <58090876+yosbel-penate@users.noreply.github.com>
2022-12-30 16:29:16 -0500

    +++

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 import { Routes, Route } from "react-router-dom"
 
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigationComponent/MainNavigations";
 import Users from './user/pages/Users';
 
 const App = () => {
   return (
     <div className="App">
-      <Routes>
-          <Route path="/" element={ <Users/> } />
-          <Route path="/place/new" element={ <NewPlace/> } />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+            <Route path="/" element={ <Users/> } />
+            <Route path="/place/new" element={ <NewPlace/> } />
+        </Routes>
+      </main>
     </div>
   )
 }
--- src/user/components/UserListComponent/UserList.js
+++ src/user/components/UserListComponent/UserList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import UsersItem from '../UserItemComponent/UserItem';
 import './UserList.css'
 
