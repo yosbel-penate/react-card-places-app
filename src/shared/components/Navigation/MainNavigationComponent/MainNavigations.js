174de5767058731ffd0f32904fafc174b96d8c7f
Corso <58090876+yosbel-penate@users.noreply.github.com>
2022-12-30 16:29:01 -0500

    tools animated menu

---
+++ src/shared/components/Navigation/MainNavigationComponent/MainNavigations.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import MainHeader from '../MainHeaderComponent/MainHeader'
+import { Link } from 'react-router-dom'
+
+import './MainNavigation.css'
+import NavLinks from '../NavLinksComponent/NavLinks'
+import SideDrawer from '../SideDrawerComponent/SideDrawer'
+import Backdrop from '../../UIElements/BackdropComponent/Backdrop'
+
+const MainNavigation = () => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+  const openDrawer = () => {
+    setDrawerIsOpen(true)
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false)
+  };
+
+  return (
+    <React.Fragment>
+        {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+          <SideDrawer show={drawerIsOpen}>
+            <nav className="main-navigation__drawer-nav">
+              <NavLinks/>
+            </nav>
+          </SideDrawer>
+        <MainHeader>
+            <button className= "main-navigation__menu-btn" onClick={openDrawer}>
+                <span/>
+                <span/>
+                <span/>
+            </button>
+            <h1 className="main-navigation__title">
+                <Link to="/"> YouPlaces</Link>
+            </h1>
+            <nav className="main-navigation__header-nav">
+                <NavLinks/>
+            </nav>
+        </MainHeader>
+    </React.Fragment>
+  )
+}
+
+export default MainNavigation
